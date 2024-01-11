0895711d9892f5aa49588387f08c2617a807da24
Brice G <bricegoudalo@gmail.com>
2024-01-11 19:35:00 +0100

    Add printPyramid function to exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,7 @@
 def main():
     x = getInt()
     print(f"This number is {x}")
+    printPyramid(3)
     
     
 # end def
@@ -12,6 +13,12 @@ def getInt():
         except ValueError:
             pass
 # end def
+
+
+def printPyramid(n):
+    for i in range(n):
+        print("#" * (i + 1))
+    # end for
 
 main()
 
