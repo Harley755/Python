795784aa2761f84ec8fa30e34742bce499ee8add
Brice G <bricegoudalo@gmail.com>
2024-01-11 18:41:17 +0100

    Refactor getInt function in exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,10 +1,13 @@
-while True:
-    try:
-        number = int(input("Please enter number : "))
-    except ValueError:
-        print("This number is not an integer")
-    else:
-        break
-    # end try
-    
-print(f"This number is {number}")
+def getInt():
+    while True:
+        try:
+            number = int(input("Please enter number : "))
+        except ValueError:
+            print("This number is not an integer")
+        else:
+            break
+        # end try
+    return number
+# end def
+
+print(f"This number is {getInt()}")
