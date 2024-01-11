3ea099339f77d94ec4a10aa2f9b1a4554e9872a8
Brice G <bricegoudalo@gmail.com>
2024-01-11 18:33:12 +0100

    Refactor exception handling in exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,7 +1,10 @@
-try:
-    number = int(input("Please enter number : "))
-except ValueError:
-    print("This number is not an integer")
-else:
-    print(f"This number is {number}")
-# end try
+while True:
+    try:
+        number = int(input("Please enter number : "))
+    except ValueError:
+        print("This number is not an integer")
+    else:
+        break
+    # end try
+    
+print(f"This number is {number}")
